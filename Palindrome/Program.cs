2183d3fb1fc8528458fe4f37e028ee3b47520263
Bernard Braun da Silva <77727808+BernardBraun@users.noreply.github.com>
2021-11-11 09:27:59 -0300

    Uploading Palindrome solution code

---
+++ Palindrome/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printResult("ADA");
+            printResult("ABCD");
+            printResult("RENNER");
+            printResult("socorram me subi no on ibus em marrocos");
+            printResult("ARARA");
+            printResult("atabaca");
+        }
+
+        public static void printResult(string word)
+        {
+            Console.WriteLine(word + " is an Palindrome? " + palindromeTest(word));
+        }
+        public static bool palindromeTest(string word)
+        {
+            Stack stack = new Stack();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                stack.Push(word[i]);
+            }
+            var inverseWord = "";
+            while (stack.Count >=1 )
+            {
+                inverseWord += stack.Pop();
+            }
+            if (inverseWord.Equals(word))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
